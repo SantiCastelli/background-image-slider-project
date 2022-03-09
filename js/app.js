1068940b934ea6e9f4679045f6ad556bd4cd3a3e
SantiCastelli <castellist@gmail.com>
2022-03-09 14:02:21 +0100

    cambios

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@
     "contBcg-3",
     "contBcg-4"
   ];
+  
+
+  let derecha = document.querySelector(".btn-right");
+  let izquierda = document.querySelector(".btn-left");
+  let imagenFondo = document.querySelector("#fondo")
+
+  
+
+  izquierda.addEventListener("click", function(){
+    console.log("muevo a izquierda");
+    derecha.style.backgroundcolor = "black";
+    
+  })
+
+  derecha.addEventListener("click", function(){
+    console.log("muevo a derecha");
+  } )
